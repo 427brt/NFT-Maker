6c1370adf876b8b2aa9acf897b63e71d99d75859
427brt <yu0427suzuki@icloud.com>
2022-10-23 06:46:13 +0900

    upload to github

--- ipfs-nfts/hardhat.config.js
+++ ipfs-nfts/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/Y88815HJqitvX9Jr71bqC4pw5XnJKCKJ",
-      accounts: ["f53f4a2741ddf0357677a5f1be368516453f7b8290af3ff004dfd20a38bfd31e"],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
